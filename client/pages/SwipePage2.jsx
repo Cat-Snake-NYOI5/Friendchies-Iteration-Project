a899efb1ca48b5ba0a58b9171feaee47fa26a216
Bennett <drbmgit@gmail.com>
2023-06-21 14:05:37 -0400

    slices

---
+++ client/pages/SwipePage2.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ACTION } from '../features/matchSlice'
+
+const SwipePage = () => {
+  const matches = useSelector((state) => state.matches);
+  const useDispatch = useDispatch();
+
+  const
+
+  return (
+    <div className="container">
+      <div className="profile-pic">
+
+      </div>
+      <div className="pet-name">
+
+      </div>
+      <div className="pet-bio">
+
+      </div>
+      <button
+        className="goBack"
+        onClick={() => handleBack(FILLER)}
+        > back </button>
+      <button className="goForward"
+        onClick={() => handleForward(FILLER)}
+        ></button>
+      <button
+        className="dislike"
+        onClick={() => handleDislike(FILLER)}
+        ></button>
+      <button className="like"
+        onCanPlay={() => handleLike(FILLER)}
+        ></button>
+    </div>
+  )
+}
+
+export default SwipePage;
