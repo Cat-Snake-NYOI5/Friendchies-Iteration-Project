d5e50c6fc24e9f09aedd155d8fe8a54dbd2f152f
Bennett <drbmgit@gmail.com>
2023-06-22 16:11:16 -0400

    createProf page

--- client/pages/CreateProfile.jsx
+++ client/pages/CreateProfile.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import logo from '../img/logo.png';
 import {
   dogName,
   ownerName,
@@ -24,74 +25,54 @@ export default function userInput() {
   // const onSubmit = (data) => {
   //   dispatch(createProf(data));
   // };
+  const nameDog = useSelector((state) => state.createProfile.dog_name);
+  const nameOwner = useSelector((state) => state.createProfile.owner_name);
+  const zipC = useSelector((state) => state.createProfile.zip);
+  const breedDog = useSelector((state) => state.createProfile.breed);
+  const sizeDog = useSelector((state) => state.createProfile.size);
+  const ageDog = useSelector((state) => state.createProfile.age);
+  const phoneDog = useSelector((state) => state.createProfile.phone_number);
+  const genderDog = useSelector((state) => state.createProfile.gender);
+  const imageDog = useSelector((state) => state.createProfile.image_url);
+  const fixedDog = useSelector((state) => state.createProfile.isfixed);
+  const bioDog = useSelector((state) => state.createProfile.biography);
+
   const handleChange = async (e) => {
     e.preventDefault();
     // console.log('I am here testing create profile')
 
-    // const value = e.target.value;
-    // const name = e.target.name;
-    // if (name === 'dog_name') {
-    //   dispatch(dogName(value));
-    // }
-    // if (name === 'owner_name') {
-    //   dispatch(ownerName(value));
-    // }
-    // if (name === 'zipcode') {
-    //   dispatch(zip(value));
-    // }
-    // if (name === 'breed') {
-    //   dispatch(breed(value));
-    // }
-    // if (name === 'size') {
-    //   dispatch(size(value));
-    // }
-    // if (name === 'age') {
-    //   dispatch(age(value));
-    // }
-    // if (name === 'phoneNumber') {
-    //   dispatch(phone(value));
-    // }
-    // if (name === 'gender') {
-    //   dispatch(gender(value));
-    // }
-    // if (name === 'dogimage') {
-    //   dispatch(image(value));
-    // }
-    // if (name === 'isFixed') {
-    //   dispatch(isfixed(value));
-    // }
-    // if (name === 'biography') {
-    //   dispatch(biography(value));
-    // }
+
     try {
+      // console.log('this is dog name', id)
+
       const result = await axios.post('http://localhost:3000/createprofile', {
-        dog_name: dogName,
-        owner_name: ownerName,
-        zip: zip,
-        breed: breed,
-        size: size,
-        age: age,
-        phone_number: phone,
-        gender: gender,
-        image_url: image,
-        isfixed: fixed,
-        biography: biography,
+        dog_name: nameDog,
+        owner_name: nameOwner,
+        zip: zipC,
+        breed: breedDog,
+        size: sizeDog,
+        age: ageDog,
+        phone_number: phoneDog,
+        gender: genderDog,
+        image_url: imageDog,
+        isfixed: fixedDog,
+        biography: bioDog,
       });
       console.log('this is my fetch', result);
 
       if (result) {
-        dispatch(dogName({ dog_name: dogName }));
-        dispatch(ownerName({ owner_name: ownerName }));
-        dispatch(zip({ zip: zip }));
-        dispatch(breed({ breed: breed }));
+        dispatch(dogName({ dog_name: nameDog }));
+        dispatch(ownerName({ owner_name: nameOwner }));
+        dispatch(zip({ zip: zipC }));
+        dispatch(breed({ breed: breedDog }));
 
-        dispatch(size({ size: size }));
-        dispatch(age({ age: age }));
-        dispatch(phone({ phone_number: phone }));
-        dispatch(gender({ gender: gender }));
-        dispatch(image({ image_url: image }));
-        dispatch(fixed({ isfixed: fixed }));
-        dispatch(bio({ biography: biography }));
+        dispatch(size({ size: sizeDog }));
+        dispatch(age({ age: ageDog }));
+        dispatch(phone({ phone_number: phoneDog }));
+        dispatch(gender({ gender: genderDog }));
+        dispatch(image({ image_url: imageDog }));
+        dispatch(fixed({ isfixed: fixedDog }));
+        dispatch(biography({ biography: bioDog }));
         // id: result.data,
       }
     } catch (error) {
@@ -100,75 +81,66 @@ export default function userInput() {
   };
 
   return (
-    <div className='userProfile'>
-      <form className='profile' onSubmit={(e) => handleChange(e)}>
+    <div className="login">
+      <img className="logo" src={logo} />
+      <h2>Create Profile</h2>
+      <form className="prof-form" onSubmit={(e) => handleChange(e)}>
         <input
           type='name'
           placeholder='Dog Name'
           // value={dog_name}
           onChange={(e) => dispatch(dogName(e.target.value))}
         />
-        <br></br>
         <input
           type='name'
           placeholder='Owner Name'
           onChange={(e) => dispatch(ownerName(e.target.value))}
         />
-        <br></br>
         <input
           type='number'
           placeholder='Zipcode'
           onChange={(e) => dispatch(zip(e.target.value))}
         />
-        <br></br>
         <input
           type='string'
           placeholder='Breed'
           onChange={(e) => dispatch(breed(e.target.value))}
         />
-        <br></br>
         <input
           type='text'
           placeholder='Size'
           onChange={(e) => dispatch(size(e.target.value))}
         />
-        <br></br>
         <input
           type='number'
           placeholder='Age'
           onChange={(e) => dispatch(age(e.target.value))}
         />
-        <br></br>
         <input
           type='number'
           placeholder='Phone Number'
           onChange={(e) => dispatch(phone(e.target.value))}
         />
-        <br></br>
         <input
           type='text'
           placeholder='Gender'
           onChange={(e) => dispatch(gender(e.target.value))}
         />
-        <br></br>
         <input
           type='text'
           placeholder='Image'
           onChange={(e) => dispatch(image(e.target.value))}
         />
-        <br></br>
         <input
           type='boolean'
           placeholder='Fixed?'
           onChange={(e) => dispatch(fixed(e.target.value))}
         />
-        <br></br>
         <input
           type='text'
           placeholder='Biography'
           onChange={(e) => dispatch(biography(e.target.value))}
         />
-        <br></br>
 
         <button type='submit' className='submit-btn'>
           Submit
